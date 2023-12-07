0947ec59afbf98ef859835355d27b79e0a52f495
shyam372 <www.shyamsundergouda1@gmail.com>
2023-12-07 15:48:17 +0530

    quiz app finished

---
+++ app/src/main/java/com/example/quizapp/QuestionAnswer.java
@@ -0,0 +1,34 @@
+package com.example.quizapp;
+
+public class QuestionAnswer {
+    public static String question[]={
+            "Which of the following user-defined header file extension used in c++?",
+            "wWhich one is not a programming language?",
+            "What is the extension of java code files?",
+            "Which of the following is not an OOPS concept in Java",
+            "Which of the following correctly declares an array in C++?",
+            "Which one of the following is not a Java feature?",
+            "Which is more effective while calling the C++ functions?"
+    };
+    public static String choices[][]={
+            {"a) hg","b) h","c) cpp","d) hf"},
+    {"a) notepad","b) java","c) dart","d) python"},
+    {"a) .js","b) .txt","c) .class","d) .java"},
+            {"a) Polymorphism","b) Inheritance","c) Compilation","d) Encapsulation"},
+            {"a) array{10};","b) array array[10];","c) int array;","d) int array[10];"},
+            {"a) Object-oriented","b) Use of pointers","c) Portable","d) Dynamic and Extensible"},
+            {"a) call by object","b) call by pointer","c) call by reference","d) call by value"}
+    };
+    public static String correctAnswers[]={
+            "b) h",
+            "a) notepad",
+           "d) .java",
+            "d) Encapsulation",
+            "d) int array[10];",
+            "b) Use of pointers",
+            "c) call by reference"
+    };
+
+
+
+}
